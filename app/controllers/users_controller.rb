b8714e99c08539db2e8ed1992566352aa1824603
Paul Dragich <paul-dragich@uiowa.edu>
2016-09-28 05:17:47 +0000

    think im done

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,32 @@
+class UsersController < ApplicationController
+    def user_params
+        params.require(:user).permit(:user_id, :email, :session_token)
+    end
+    
+    def index
+        @users = User.all
+    end
+    
+    def show
+        id = params[:id] # retrieve movie ID from URI route
+        @user = User.find(id) # look up movie by unique ID
+        # will render app/views/movies/show.<extension> by default
+    end
+    
+    def create
+        @user = params[:user_id] # retrieve movie ID from URI route
+        if (User.exists?(:user_id => user_params[:user_id]))
+            flash[:notice] = "Sorry this user_id is taken. Try again."
+            redirect_to new_user_path
+        else
+            User.create_user!(user_params)
+            flash[:notice] = "Welcome #{user_params[:user_id]}. You account has been created."
+            #@current_user ||= session[:session_token ] && User.find_by_session_token(session[:session_token])
+            redirect_to login_path
+        end
+        
+   end
+    
+    def new
+    end
+end
